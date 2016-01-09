e9a7b60cb1ee433a33e0ef4510ecef0c82c135ad
Jérôme Ceccato <jerome.ceccato@gmail.com>
2016-01-09 18:32:48 +0100

    Added formatter

--- test/project/src/js/format.js
+++ test/project/src/js/format.js
@@ -4,6 +4,40 @@
 
 var Formatter = {
     number: function (n) {
-        return n.toString();
+        return this.suffixNotation(n.floor());
+    },
+    
+    suffixNotation: function (n) {
+        var nString = n.toFixed();
+        if (nString.length < 4) {
+            return nString;
+        }
+
+        // Taken from https://github.com/swarmsim/swarm/blob/master/app/scripts/filters/bignum.coffee
+        var suffixes = ['', 'K', 'M', 'B', 'T', 'Qa', 'Qi', 'Sx', 'Sp', 'Oc', 'No',
+        'Dc', 'UDc', 'DDc', 'TDc', 'QaDc', 'QiDc', 'SxDc', 'SpDc', 'ODc', 'NDc',
+        'Vi', 'UVi', 'DVi', 'TVi', 'QaVi', 'QiVi', 'SxVi', 'SpVi', 'OVi', 'NVi',
+        'Tg', 'UTg', 'DTg', 'TTg', 'QaTg', 'QiTg', 'SxTg', 'SpTg', 'OTg', 'NTg',
+        'Qd', 'UQd', 'DQd', 'TQd', 'QaQd', 'QiQd', 'SxQd', 'SpQd', 'OQd', 'NQd',
+        'Qq', 'UQq', 'DQq', 'TQq', 'QaQq', 'QiQq', 'SxQq', 'SpQq', 'OQq', 'NQq',
+        'Sg', 'USg', 'DSg', 'TSg', 'QaSg', 'QiSg', 'SxSg', 'SpSg', 'OSg', 'NSg',
+        'St', 'USt', 'DSt', 'TSt', 'QaSt', 'QiSt', 'SxSt', 'SpSt', 'OSt', 'NSt',
+        'Og', 'UOg', 'DOg', 'TOg', 'QaOg', 'QiOg', 'SxOg', 'SpOg', 'OOg', 'NOg'];
+
+        var significantDigits = 3;
+        var power = Math.floor((nString.length - 1) / 3);
+        var integerPart = nString.length - (power * 3);
+        if (power < suffixes.length) {
+            var result = nString.substr(0, integerPart);
+            if (integerPart < significantDigits) {
+                result += '.' + nString.substr(integerPart, significantDigits - integerPart)
+            }
+            return result + suffixes[power];
+        }
+        return this.exponentNotation(n);
+    },
+
+    exponentNotation: function (n) {
+        return n.toExponential(2).replace('+', '');
     }
 };
